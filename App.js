627924e8082ac016a1479d3cd432db4a98f85862
Nichole Courtney <nichole.a.courtney@gmail.com>
2022-10-30 13:31:26 -0600

    Update: Book example working. Picker installed.

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Picker } from "@react-native-picker/picker";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const key = "@MyApp:key";
+
+export default class App extends Component {
+  state = {
+    text: "",
+    storedValue: "",
+  };
+
+  componentWillMount() {
+    this.onLoad();
+  }
+
+  onLoad = async () => {
+    try {
+      const storedValue = await AsyncStorage.getItem(key);
+      this.setState({ storedValue });
+    } catch (error) {
+      Alert.alert("Error", "There was an error while loading the data");
+    }
+  };
+
+  onSave = async () => {
+    const { text } = this.state;
+
+    try {
+      await AsyncStorage.setItem(key, text);
+      Alert.alert("Saved", "Successfully saved on device");
+    } catch (error) {
+      Alert.alert("Error", "There was an error while saving the data");
+    }
+  };
+
+  onChange = (text) => {
+    this.setState({ text });
+  };
+
+  render() {
+    const { storedValue, text } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.preview}>{storedValue}</Text>
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={this.onChange}
+            value={text}
+            placeholder="Type something here..."
+          />
+          <TouchableOpacity onPress={this.onSave} style={styles.button}>
+            <Text>Save locally</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onLoad} style={styles.button}>
+            <Text>Load data</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  preview: {
+    backgroundColor: "#bdc3c7",
+    width: 300,
+    height: 80,
+    padding: 10,
+    borderRadius: 5,
+    color: "#333",
+    marginBottom: 50,
+  },
+  input: {
+    backgroundColor: "#ecf0f1",
+    borderRadius: 3,
+    width: 300,
+    height: 40,
+    padding: 5,
+  },
+  button: {
+    backgroundColor: "#f39c12",
+    padding: 10,
+    borderRadius: 3,
+    marginTop: 10,
   },
 });
